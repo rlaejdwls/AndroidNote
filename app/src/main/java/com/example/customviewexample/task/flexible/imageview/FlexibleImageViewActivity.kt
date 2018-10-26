2f77f243c5223496b54304dca872d3107a73b976
uitaek <uitaek@tigrison.com>
2018-10-26 09:53:25 +0900

    - 뷰의 visibility 속성 이용해서 레이아웃이 바뀌는 예제

---
+++ app/src/main/java/com/example/customviewexample/task/flexible/imageview/FlexibleImageViewActivity.kt
@@ -0,0 +1,17 @@
+package com.example.customviewexample.task.flexible.imageview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customviewexample.R
+
+/**
+ * Created by Hwang on 2018-10-12.
+ *
+ * Description :
+ */
+class FlexibleImageViewActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flexible_image_view)
+    }
+}
